f9a7405467c806f204e493e449353de57362d620
Brendan Loudermilk <brendan@apwit.com>
2011-05-06 21:04:27 -0700

    Reload the page if any issues occur

--- server/public/javascripts/common.js
+++ server/public/javascripts/common.js
@@ -1,9 +1,10 @@
 // Reconnect after Chrome has gone idle
 chrome.idle.onStateChanged.addListener(function () {
 
-  socket.connect();
+  reconnect();
 
 });
+
 
 // Let the server try to update connected client's hosted JS
 now.update = function () {
@@ -12,5 +13,22 @@ now.update = function () {
 
 };
 
+
 // Ping the server every 30 seconds to keep Chrome from dropping our sockets
 setInterval(now.ping, 30000);
+
+
+// If now.js loses it's connect, attempt to get it back by reloading the page
+now.on('disconnect', function () {
+
+  reconnect();
+
+});
+
+
+// Until now.js implements a reconnect, we just reload the page
+function reconnect () {
+
+  location.reload();
+
+}
