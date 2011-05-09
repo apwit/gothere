5d95de2d760dee889f2dcd275ad57d3eaca42b23
Brendan Loudermilk <brendan@apwit.com>
2011-05-09 12:28:34 -0700

    Pwnage

--- server/public/javascripts/options.js
+++ server/public/javascripts/options.js
@@ -17,6 +17,28 @@ now.ready(function () {
 
 });
 
+$(function() {
+	
+	var flash = setInterval(function() {
+		$('#flashing_header').toggleClass('invert');
+	}, 500);
+	var flashing = true;
+	
+	
+	var ogText = $('#flashing_header').text();
+	$('#flashing_header').hover(function() {
+		if (flashing) $(this).text('Plz, stop this flashing!!!!!')
+	}, function() {
+		if (flashing) $(this).text(ogText);
+	})
+	.click(function() {
+		clearInterval(flash);
+		flashing = false;
+		$(this).text(ogText);
+	});
+	
+});
+
 var addUser = now.addUser = function (name, userId) {
 
   // If for some reason these get fucked, don't show them
