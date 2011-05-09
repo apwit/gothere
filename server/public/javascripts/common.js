188b4c751d194bd2cae13a0ad4426cf8a60450db
Brendan Loudermilk <brendan@apwit.com>
2011-05-08 22:11:52 -0700

    Log the connected state of the socket

--- server/public/javascripts/common.js
+++ server/public/javascripts/common.js
@@ -1,26 +1,5 @@
-// Reconnect after Chrome has gone idle
-chrome.idle.onStateChanged.addListener(reload);
+setInterval(function () {
 
+  console.log(socket.connected);
 
-// Let the server try to update connected client's hosted JS
-now.update = reload;
-
-
-function bindSocketDisconnect () {
-
-  // If now.js loses it's connect, attempt to get it back by reloading the page
-  socket.on('disconnect', reload);
-
-}
-
-
-// Simply reloads the page
-function reload () {
-
-  setTimeout(function () {
-
-    location.reload();
-
-  }, 1000);
-
-}
+}, 1000);
